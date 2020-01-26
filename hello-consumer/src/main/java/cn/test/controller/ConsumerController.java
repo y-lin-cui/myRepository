9ad7513809248898751fffd24006ca5d4840c65b
admin <admin@LAPTOP-CPQU52N2>
2020-01-27 02:46:11 +0800

    添加断路器

--- hello-consumer/src/main/java/cn/test/controller/ConsumerController.java
+++ hello-consumer/src/main/java/cn/test/controller/ConsumerController.java
@@ -5,23 +5,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import cn.test.service.ConsumerService;
+
 /**
-* @author:ylin
-* @tips:
-* @version 1
-* @Date:上午10:18:04
-*/
+ * @author:ylin
+ * @tips:
+ * @version 1
+ * @Date:上午10:18:04
+ */
 @RestController
 public class ConsumerController {
 
-    //这里注入的restTemplate就是在com.sam.ConsumerApp中通过@Bean配置的实例
-    @Autowired
-    RestTemplate restTemplate;
+	@Autowired
+	ConsumerService service;
 
-    @RequestMapping("/hello-consumer")
-    public String helloConsumer() {
-        //调用hello-service服务，注意这里用的是服务名，而不是具体的ip+port
-        restTemplate.getForObject("http://hello-service/hello", String.class);
-        return "hello consumer finish !!!";
-    }
+	@RequestMapping("/hello-consumer")
+	public String helloConsumer() {
+		// //调用hello-service服务，注意这里用的是服务名，而不是具体的ip+port
+		// restTemplate.getForObject("http://hello-service/hello",
+		// String.class);
+		return service.consumer();
+	}
 }
--- hello-consumer/src/main/java/cn/test/service/ConsumerService.java
+++ hello-consumer/src/main/java/cn/test/service/ConsumerService.java
@@ -1,6 +1,10 @@
 package cn.test.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
 * @author:ylin
@@ -10,5 +14,18 @@ import org.springframework.stereotype.Service;
 */
 @Service
 public class ConsumerService {
+	
+	@Autowired
+    RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "errorMsg")
+    public String consumer() {
+        // 调用hello-service服务，注意这里用的是服务名，而不是具体的ip+port
+        restTemplate.getForObject("http://hello-service/hello", String.class);
+        return "hello consumer finish !!!";
+    }
+
+    public String errorMsg() {
+        return "hello-service某节点已挂，请尝试刷新";
+    }
 }
