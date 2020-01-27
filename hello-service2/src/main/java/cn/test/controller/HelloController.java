b562dea6fcfba0a35a2f97cff22c8c992ad4dda9
admin <admin@LAPTOP-CPQU52N2>
2020-01-27 14:39:59 +0800

    新增生产者方法

--- hello-service2/src/main/java/cn/test/controller/HelloController.java
+++ hello-service2/src/main/java/cn/test/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -28,5 +29,10 @@ public class HelloController {
 	        //打印服务的服务id
 	        logger.info("*********" + instance.getServiceId());
 	        return "hello,this is hello-service";
+	    }
+	    
+	    @RequestMapping( value = "/testProducer")
+	    public String testProducer( @RequestParam String param ){
+	        return "生产者接2收消息： " + param;
 	    }
 }
