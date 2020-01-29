0d5cbf29d22e0cf4eba08a5ec0fce0bda0eaa119
admin <admin@LAPTOP-CPQU52N2>
2020-01-30 02:19:57 +0800

    开启过滤拦截

--- api-gateway/src/main/java/cn/test/GateWayApp.java
+++ api-gateway/src/main/java/cn/test/GateWayApp.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import cn.test.back.filter.AccessFilter;
 
 /**
 * @author:ylin
@@ -19,7 +22,13 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringCloudApplication
 @EnableDiscoveryClient
 public class GateWayApp {
-
+	
+	 //追加bean的实现 开启过滤
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+	
     public static void main(String[] args) {
         SpringApplication.run(GateWayApp.class, args);
     }
